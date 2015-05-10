e036aa8d781c38dfe41b0c78cc2e9c8d730ca85d
Toshio Ito <debug.ito@gmail.com>
2015-05-10 18:20:58 +0900

    Add Synopsis.

---
+++ eg/synopsis.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+import System.IO (putStrLn)
+import Control.Concurrent (threadDelay)
+
+import qualified Control.FoldDebounce as Fdeb
+
+printCount :: Int -> IO ()
+printCount v = putStrLn ("value = " ++ show v)
+
+main :: IO ()
+main = do
+  trigger <- Fdeb.new Fdeb.Args { Fdeb.cb = printCount, Fdeb.fold = (+), Fdeb.init = 0 }
+                      Fdeb.def { Fdeb.delay = 500000 }
+  trigger 1
+  trigger 2
+  trigger 3
+  threadDelay 1000000 -- During this, "value = 6" is printed.
+  trigger 4
+  threadDelay 1000    -- Nothing is printed.
+  trigger 5
+  threadDelay 1000000 -- During this, "value = 9" is printed.
+  
