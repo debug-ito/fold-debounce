42e76a20d08e1011417e8cb066a083aa80d2f0a3
Toshio Ito <debug.ito@gmail.com>
2015-05-10 22:39:46 +0900

    add a simple failing test.

--- test/Control/FoldDebounceSpec.hs
+++ test/Control/FoldDebounceSpec.hs
@@ -1,12 +1,22 @@
 module Control.FoldDebounceSpec (main, spec) where
 
+import Control.Concurrent.Chan (Chan,newChan,writeChan,readChan)
 import Test.Hspec
+import qualified Control.FoldDebounce as F
 
 main :: IO ()
 main = hspec spec
 
+forFIFO :: ([Int] -> IO ()) -> F.Args Int [Int]
+forFIFO cb = F.Args {
+  F.cb = cb, F.fold = (\l v -> l ++ [v]), F.init = []
+  }
+
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
-      True `shouldBe` False
+  describe "new" $ do
+    it "emits single output event for single input event" $ do
+      output <- newChan
+      trig <- F.new (forFIFO $ writeChan output) F.def { F.delay = 5000 }
+      trig 10
+      readChan output `shouldReturn` [10]
