480fdabd06932e527c857888ea43578f17535f8a
Toshio Ito <debug.ito@gmail.com>
2015-05-13 22:22:30 +0900

    FoldDebounceSpec: little refactoring.

--- test/Control/FoldDebounceSpec.hs
+++ test/Control/FoldDebounceSpec.hs
@@ -1,6 +1,6 @@
 module Control.FoldDebounceSpec (main, spec) where
 
-import Control.Concurrent.Chan (newChan,writeChan,readChan)
+import Control.Concurrent.Chan (Chan,newChan,writeChan,readChan)
 import Test.Hspec
 import qualified Control.FoldDebounce as F
 
@@ -12,12 +12,15 @@ forFIFO cb = F.Args {
   F.cb = cb, F.fold = (\l v -> l ++ [v]), F.init = []
   }
 
+fifoTrigger :: Chan [Int] -> F.Opts Int [Int] -> IO (F.Trigger Int)
+fifoTrigger output opts = F.new (forFIFO $ writeChan output) opts
+
 spec :: Spec
 spec = do
   describe "new" $ do
     it "emits single output event for single input event" $ do
       output <- newChan
-      trig <- F.new (forFIFO $ writeChan output) F.def { F.delay = 5000 }
+      trig <- fifoTrigger output F.def { F.delay = 50000 }
       F.send trig 10
       readChan output `shouldReturn` [10]
       F.close trig
